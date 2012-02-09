360c6fc38fd1c7d05eff4998fe9525ce2cb1a175
Alex Punco <a.s.punco@gmail.com>
2012-02-09 17:14:22 +0400

    begining

---
+++ creator.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+require "rubygems"
+require "net/smtp"
+require "mysql2"
+
+
+def mail_exist?
+	
+end
+
+def domain_exist?
+	
+end
+
+def create_mail
+	
+end
+
+def initial_message
+	
+end
+
+puts "anymail"
