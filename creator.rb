ca011a8c4035d86530f1b586ad143d37ceaa3786
Alex Punco <a.s.punco@gmail.com>
2012-02-09 18:11:09 +0400

    del main conf

--- creator.rb
+++ creator.rb
@@ -4,10 +4,6 @@ require "net/smtp"
 require "mysql2"
 require 'yaml'
 
-def initialize
-	config = File.open("mysql_config.yaml", "r")
-end
-
 def mail_exist?
 	
 end
@@ -24,4 +20,18 @@ def initial_message
 	
 end
 
+def connect_mysql
+	config = File.open("mysql_config.yaml")
+	yp = YAML::load_documents( config ) { |param|
+  	puts "#{param['hostname']} #{param['database']} #{param['username']} #{param['password']}"
+  	@mysql_connection = Mysql2::Client.new(:host => "#{param['hostname']}",
+  										   :username => "#{param['username']}",
+  										   :password => "#{param['password']}", 
+  										   :database => "#{param['database']}")
+	}
+	p yp	
+end
 puts "anymail"
+
+connect_mysql
+
