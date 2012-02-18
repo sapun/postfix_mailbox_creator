2626601e44725173a73c921226a94f6932c58b92
Alexander Punco <sapun@guitarplayer.ru>
2012-02-18 20:12:59 +0400

    any new shit. script unwork yet, debug start

--- creator.rb
+++ creator.rb
@@ -20,42 +20,57 @@ class Creter_mails
 		@password_now = random_password
 		@body_hello_message = "now empty"
 	end
-	
+
+	def create_mail
+		connect_mysql
+		p @mysql_connection
+		result = @mysql_connection.query("SELECT * FROM `domain` WHERE `domain` = 'gorod-skidokdd.com' ").count
+		p result
+		#result.each { |e| puts e }
+		a = mail_exist?
+		if mail_exist? == false
+			puts "this mail alredy exists"
+			exit
+		end
+		if domain_exist?
+			puts "this domain unregistred"
+			exit
+		end	
+		@mysql_connection.query("INSERT INTO `postfix`.`mailbox` (`username`, `password`, `name`, `maildir`, `quota`,
+							 `local_part`, `domain`, `created`, `modified`,
+							 `active`) VALUES ('@email', '@password_now',
+							 '@local_part', '@domain/@@email/', '0',
+							  '@local_part', '@domain', 'Time.now', 'Time.now', '1'")
 		
+	end
+
+	protected
 	def mail_exist?
-		if @connect_mysql.query("SELECT * FROM mailbox WHERE username='#{@email}'").nil?
+		if @mysql_connection.query("SELECT * FROM mailbox WHERE username='#{@email}'").count > 0
 			return false
 		end
 		return true
 	end
 
 	def domain_exist?
-		if @connect_mysql.query("SELECT * FROM domain WHERE domain= '#{@domain}'").nil?
+		if @mysql_connection.query("SELECT * FROM domain WHERE domain= '#{@domain}'").count > 0
 			return false
 		end
 		return true
 	end
 
-	def create_mail
-		@connect_mysql.query("INSERT INTO `postfix`.`mailbox` (`username`, `password`, `name`, `maildir`, `quota`,
-							 `local_part`, `domain`, `created`, `modified`,
-							 `active`) VALUES ('@email', '@password_now',
-							 '@local_part', '@domain/@@email/', '0',
-							  '@local_part', '@domain', Time.now, 'Time.now', '1'")
-		
-	end
-
 	def connect_mysql
 		config = File.open("mysql_config.yaml")
 		yp = YAML::load_documents( config ) { |param|
-	  	puts "#{param['hostname']} #{param['database']} #{param['username']} #{param['password']}"
+	  	#puts "#{param['hostname']} #{param['database']} #{param['username']} #{param['password']}"
 	  	@mysql_connection = Mysql2::Client.new(:host => "#{param['hostname']}",
 	  										   :username => "#{param['username']}",
 	  										   :password => "#{param['password']}", 
 	  										   :database => "#{param['database']}")
 		}
-		p yp	
+		#p yp	
 	end	
+
 	def initial_message
 		config = File.open("smtp_config.yaml")
 		yp = YAML::load_documents( config ) { |param|
@@ -65,6 +80,7 @@ class Creter_mails
         	smtp.finish
         }
 	end
+
 	def random_password(size = 10)
   		chars = (('a'..'z').to_a + ('0'..'9').to_a) - %w(i o 0 1 l 0)
   		(1..size).collect{|a| chars[rand(chars.size)] }.join
@@ -80,4 +96,5 @@ else
 	exit
 end
 
+email.create_mail
 	
