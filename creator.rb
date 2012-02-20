aba1ae3db7c95befa277099e5e1f278b577fa306
Konstantin Rafalsky <kraft001@gmail.com>
2012-02-20 16:23:02 +0400

    make queries more readable

--- creator.rb
+++ creator.rb
@@ -28,22 +28,31 @@ END_OF_MESSAGE
 
 	def create_mail
 		connect_mysql
-		if mail_exist? == false
-			puts "this mail alredy exists"
-			exit
-		end
-		if domain_exist?
-			puts "this domain unregistred"
-			exit
-		end
-		@mysql_connection.query("INSERT INTO mailbox (username, password, name,
-								maildir, quota,local_part, domain, created, modified,
-							 	active) VALUES ('#{@email}', '#{@password_now}',
-							 	'#{@local_part}', '#{@maildir}', '0','#{@local_part}',
-							 	'#{@domain}', '#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}',
-							 	'#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}','1')")
 
-		@mysql_connection.query("INSERT INTO alias (address, goto,domain, created, modified, active)
+		puts "this mail alredy exists" and exit if mail_exist?
+		puts "this domain unregistred" and exit if domain_exist?
+
+		mailbox_params = {
+		  :username   => @email,
+		  :password   => @password_now,
+		  :name       => @local_part,
+			:maildir    => @maildir,
+			:quota      => 0,
+  		:local_part => @local_part,
+			:domain     => @domain,
+			:created    => Time.now.strftime("%Y-%m-%d %H:%M:%S"),
+			:modified   => Time.now.strftime("%Y-%m-%d %H:%M:%S"),
+		 	:active     => 1
+		}
+		mailbox_fields = mailbox_params.keys.join(',')
+		mailbox_values = mailbox_params.values.map{|v| "'#{v}'"}.join(',')
+		query("
+		  INSERT INTO
+		    mailbox (#{mailbox_fields})
+		 	VALUES
+  	 	  (#{mailbox_values})")
+
+		query("INSERT INTO alias (address, goto,domain, created, modified, active)
 								VALUES ( '#{@email}', '#{@email}','#{@domain}',
 										'#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}',
 										'#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}', '1')
@@ -64,32 +73,43 @@ END_OF_MESSAGE
 
 	def connect_mysql
 		config = File.open("mysql_config.yaml")
-		yp = YAML::load_documents( config ) { |param|
-	  	@mysql_connection = Mysql2::Client.new(:host => "#{param['hostname']}",
-	  										   :username => "#{param['username']}",
-	  										   :password => "#{param['password']}",
-	  										   :database => "#{param['database']}")
-		}
+		param = YAML::load( config )
+    @mysql_connection =
+  	  Mysql2::Client.new(
+        :host     => param['hostname'],
+		    :username => param['username'],
+		    :password => param['password'].to_s,
+		    :database => param['database']
+		  )
 	end
+
 	def disconnect_mysql
 		@mysql_connection.close
 	end
+
 	def query(query_str)
 	  @mysql_connection.query(query_str)
   end
+
 	def initial_message
 		config = File.open("smtp_config.yaml")
-		yp = YAML::load_documents( config ) { |param|
-			smtp = Net::SMTP.start("#{param['smtp_server']}","#{param['port']}","#{param['smtp_server']}",
-            					    "#{param['username']}", "#{param['password']}", :login)
-        	smtp.send_message @body_hello_message, "#{param['username']}", @email
-        	smtp.finish
-        }
+		param = YAML::load( config )
+	  smtp =
+	    Net::SMTP.start(
+  		  param['smtp_server'],
+  		  param['port'],
+  		  param['smtp_server'],
+	      param['username'],
+	      param['password'],
+	      :login
+	    )
+  	smtp.send_message @body_hello_message, "#{param['username']}", @email
+  	smtp.finish
 	end
 
 	def random_password(size = 10)
-  		chars = (('a'..'z').to_a + ('0'..'9').to_a) - %w(i o 0 1 l 0)
-  		(1..size).collect{|a| chars[rand(chars.size)] }.join
+		chars = (('a'..'z').to_a + ('0'..'9').to_a) - %w(i o 0 1 l 0)
+		(1..size).collect{|a| chars[rand(chars.size)] }.join
 	end
 end
 
